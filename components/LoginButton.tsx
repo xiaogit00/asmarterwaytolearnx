595aa4e7b5a2cf7cc59779f17981bdd87a3f3274
xiaogit00 <leibingguo@gmail.com>
2023-10-31 16:14:26 +0800

    Added loading to LoginButton

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,9 +1,18 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const LoginButton = (): JSX.Element => {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
   // console.log("Session:", session)
-
+  if (status === "loading") {
+    return (
+      <>
+        {'Loading User Cards...'
+        }
+        <button >Loading...</button>
+      </>
+    )
+  }
+  
   if (session) {
     return (
       <>
