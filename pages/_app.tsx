523d1bf1b5ab5f3a49194c3a98c44089114a6205
xiaogit00 <leibingguo@gmail.com>
2023-11-11 14:00:27 +0800

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
+import Script from "next/script"
 import MainLayout from '../layouts/MainLayout'
 import { useState, useEffect } from 'react'
 import { useTopicStore } from '../store'
@@ -15,10 +16,28 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   }, [fetchTopics])
   
   return (
-    <SessionProvider session={session}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </SessionProvider>
+    <>
+      <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+        </Script>
+      <SessionProvider session={session}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </SessionProvider>
+    </>
+    
       
   )}
