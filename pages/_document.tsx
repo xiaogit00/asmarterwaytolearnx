1ecb5e4601719f95fa286e26ce883749140a6597
xiaogit00 <leibingguo@gmail.com>
2023-11-13 09:17:29 +0800

    Changed favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
       <body>
         <Main />
         <NextScript />
